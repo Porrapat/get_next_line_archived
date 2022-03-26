c6b5b888476f758a79437727aefa1f2c3a250bcf
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-03-26 17:36:42 +0700

    New Algor Function

--- get_next_line.c
+++ get_next_line.c
@@ -12,17 +12,36 @@
 
 #include "get_next_line.h"
 
+char	*check_left_str(char *left_str)
+{
+	char	*ret;
+	size_t	len;
+	if (left_str == NULL)
+	{
+		len = 0;
+		ret = NULL;
+	}
+	else
+	{
+		len = ft_strlen(left_str);
+		ret = ft_strnstr(left_str, "\n", len);
+	}
+	return (ret);
+}
+
 char	*ft_read_to_left_str(int fd, char *left_str)
 {
 	char	*buff;
 	int		rd_bytes;
+	char	*ret;
 
 	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buff)
 		return (NULL);
 	rd_bytes = 1;
-	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
-	// while (!ft_strnstr(left_str, "\n", ft_strlen(left_str)) && rd_bytes != 0)
+	// while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
+	ret = check_left_str(left_str);
+	while (!ret && rd_bytes != 0)
 	{
 		rd_bytes = read(fd, buff, BUFFER_SIZE);
 		if (rd_bytes == -1)
@@ -33,6 +52,7 @@ char	*ft_read_to_left_str(int fd, char *left_str)
 		buff[rd_bytes] = '\0';
 		// ft_strlcat(left_str, buff, ft_strlen(left_str) + ft_strlen(buff) + 1);
 		left_str = ft_strjoin(left_str, buff);
+		ret = check_left_str(left_str);
 	}
 	free(buff);
 	return (left_str);
